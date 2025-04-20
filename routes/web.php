a9e57632defd9b902f5d4db1c9e1c7e899bfc3ab
J-Gerritsen <artis650@outlook.com>
2025-04-20 23:05:08 +0200

    refactored the routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ContactController;
 use App\Http\Middleware\RoleMiddleware;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
@@ -25,8 +26,6 @@ Route::middleware(['auth', 'role:Admin,Moderator'])->group(function () {
 
 Route::get('/store', [ProductController::class, 'byCategory'])->name('products.store');
 Route::get('/product/{id}', [ProductController::class, 'show'])->name('product.show');
-
-use App\Http\Controllers\ContactController;
 
 Route::get('/contact', [ContactController::class, 'showForm'])->name('contact.form');
 Route::post('/contact', [ContactController::class, 'submit'])->name('contact.submit');
